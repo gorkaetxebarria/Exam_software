6e46d43e904a82fd64d679c27fec97c97bc27c20
gorka etxebarria <gorka.etxebarria@gmail.com>
2022-11-04 09:33:32 +0100

    add mock

--- ExamTest/src/test/java/exam/exam1_mock.java
+++ ExamTest/src/test/java/exam/exam1_mock.java
@@ -1,13 +1,28 @@
 package exam;
 
+import org.easymock.EasyMock;
 import org.easymock.EasyMockSupport;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class exam1_mock extends EasyMockSupport {
-    exam1 exam1;
+   exam1 exam1Mock;
     @Before
-    public void setUp() throws Exception {
-        exam1 = partialMockBuilder(exam1.class).addMockedMethod("oneHalf").addMockedMethod("getRestZero").createMock();
+    public  void setUp() throws Exception {
+      exam1Mock=partialMockBuilder(exam1.class).addMockedMethods("oneHalf", "getRestZero").createMock();
+    }
+
+    @Test
+    public void examMockTest() {
+        EasyMock.expect(exam1.oneHalf(2)).andReturn(1);
+        EasyMock.expect(exam1.getRestZero(2, 1)).andReturn(false);
+        replayAll();
+        assertEquals(1,exam1.oneHalf(2));
+        assertFalse(exam1.getRestZero(2, 1));
+        verifyAll();
     }
 
 
--- ExamTest/src/test/java/exam/exam1_unitTesting.java
+++ ExamTest/src/test/java/exam/exam1_unitTesting.java
@@ -1,16 +1,43 @@
 package exam;
 
-import org.junit.Before;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-@runwith(value = Parameterized.class)
-public class exam1_unitTesting {
-    exam1 exam1;
 
-    @Before
-    public void setUp() throws Exception {
-        exam1 = new exam1();
-    }
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(value = Parameterized.class)
+public class exam1_unitTesting extends TestCase {
+    int n;
+    boolean expected;
+
+
     @Parameterized.Parameters
+    public static Collection<Object[]> numbers(){
+        return List.of(new Object[][]{
+                {0,false},
+                {1,false},
+                {2,true},
+                {3,true},
+                {4,false},
+                {5,true},
+                {6,false},
+                {7,true},
+                {8,false},
+                {9,false},
+                {10,false},
+        });
+    }
 
+    public  exam1_unitTesting(int n, boolean expected) {
+        this.n = n;
+        this.expected = expected;
+    }
 
+    @Test
+    public void TestExam() {
+        assertEquals(expected, exam1.exam(n));
+    }
 }
